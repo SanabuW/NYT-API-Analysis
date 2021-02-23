3431e704cd1ed286905b6c49a25b8db310723e62
Paula Lopes <paulalopes@Paulas-MacBook-Air.local>
2021-02-22 22:16:17 -0800

    mongo dfs

---
+++ lopespaula/test_multiple_api.py
@@ -0,0 +1,21 @@
+def get_api(api):
+    x
+    x
+    x
+    x
+    x
+
+    return results
+
+
+
+api_list = ['viewed_api', 'emailed_api', 'shared_api']
+
+dict_api = {}
+for api in api_list:
+    dict_api[api] = get_api(api)
+
+document_list = []
+
+documentlist.append(get_mostpopular())
+...
