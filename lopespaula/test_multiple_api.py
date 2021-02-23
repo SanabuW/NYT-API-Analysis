02e551d68b33608a4151fda208e24dd3501914a0
Paula Lopes <paulalopes@Paulas-MacBook-Air.local>
2021-02-23 15:44:25 -0800

    mongo db for most viewed emailed and shared articles

--- lopespaula/test_multiple_api.py
+++ lopespaula/test_multiple_api.py
@@ -1,15 +1,35 @@
+# Dependencies
+from bs4 import BeautifulSoup
+import requests
+import pymongo
+
+import json
+
+# Initialize PyMongo to work with MongoDBs
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+# Define database and collection
+db = client.views_db
+collection = db.articles2
+
+# URL of page to be scraped
+mostviewed_api = 'https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?api-key=UO0KtPw9TJHhMgofDtUHBKCzivb20tkN'
+mostemailed_api = 'https://api.nytimes.com/svc/mostpopular/v2/emailed/30.json?api-key=UO0KtPw9TJHhMgofDtUHBKCzivb20tkN'
+mostshared_api = 'https://api.nytimes.com/svc/mostpopular/v2/shared/30/facebook.json?api-key=UO0KtPw9TJHhMgofDtUHBKCzivb20tkN'
+
+api_list = ['mostviewed_api', 'mostemailed_api', 'mostshared_api']
+
 def get_api(api):
-    x
-    x
-    x
-    x
-    x
+    response = requests.get(api)
 
-    return results
+    result = json.loads(response.content)
 
+    most_popular_articles = result['results']
+    most_popular_articles = [{'api': 'most_viewed' ,'title': x['title'], 'published_date': x['published_date'], 'adx_keywords': x['adx_keywords'].split(';')} for x in results]
 
+   return most_popular_articles
 
-api_list = ['viewed_api', 'emailed_api', 'shared_api']
 
 dict_api = {}
 for api in api_list:
@@ -17,5 +37,7 @@ for api in api_list:
 
 document_list = []
 
-documentlist.append(get_mostpopular())
-...
+documentlist.append(most_popular_articles())
+
+
+collection.insert_many(document_list)
