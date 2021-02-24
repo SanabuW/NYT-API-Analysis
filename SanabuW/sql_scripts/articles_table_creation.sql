96d7770244134271ba61c75f705ffb9d4cd90756
Sanabu W <sanabuwashizuka@gmail.com>
2021-02-23 17:36:35 -0800

    finished analysis

--- SanabuW/sql_scripts/articles_table_creation.sql
+++ SanabuW/sql_scripts/articles_table_creation.sql
@@ -1,4 +1,6 @@
-CREATE TABLE articles_gs (	
+CREATE TABLE articles_gs_app3 (	
+	id SERIAL PRIMARY KEY,
+	search_term VARCHAR,
 	abstract VARCHAR,
 	web_url VARCHAR,
 	main_headline VARCHAR,
@@ -11,6 +13,7 @@ CREATE TABLE articles_gs (
 	search_hits INT,
 	week_ending_in VARCHAR
 );
+
 
 -- Import data through Table Import/Export wizard with settings:
 -- Header: Yes
