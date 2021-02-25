5785dedaffdaf2c13fd571b3288e9efd56d2d920
JRandle23 <jenny.randle23@gmail.com>
2021-02-24 22:28:30 -0800

    fixing api key

--- Top_Stories API/top_stories_pymongo.py
+++ Top_Stories API/top_stories_pymongo.py
@@ -12,8 +12,8 @@ client = pymongo.MongoClient(conn)
 db = client.top_stories_nyt
 collection = db.business
 
-# URL of page to be scraped
-business_url = "https://api.nytimes.com/svc/topstories/v2/business.json?api-key=WCneLj9e31CN4wBTLqA7RxndRuHC10eZ"
+# URL of page to be scraped - PLEASE USE YOUR API HERE
+business_url = "https://api.nytimes.com/svc/topstories/v2/business.json?api-key=YOURAPI"
 
 # Retrieve Business top stories 
 response = requests.get(business_url)
@@ -33,8 +33,8 @@ collection.insert_many(results)
 db = client.top_stories_nyt
 collection = db.politics
 
-# Politics top stories url
-politics_url = "https://api.nytimes.com/svc/topstories/v2/politics.json?api-key=WCneLj9e31CN4wBTLqA7RxndRuHC10eZ"
+# Politics top stories url PLEASE USE YOUR API HERE
+politics_url = "https://api.nytimes.com/svc/topstories/v2/politics.json?api-key=YOURAPI"
 
 # Retrieve Politics top stories 
 response = requests.get(politics_url)
@@ -52,8 +52,8 @@ collection.insert_many(results)
 db = client.top_stories_nyt
 collection = db.us
 
-# US top stories URL
-us_url = "https://api.nytimes.com/svc/topstories/v2/us.json?api-key=WCneLj9e31CN4wBTLqA7RxndRuHC10eZ"
+# US top stories URL - PLEASE USE YOUR API HERE
+us_url = "https://api.nytimes.com/svc/topstories/v2/us.json?api-key=YOURAPI"
 
 # Retrieve US top stories 
 response = requests.get(us_url)
@@ -71,8 +71,8 @@ collection.insert_many(results)
 db = client.top_stories_nyt
 collection = db.technology 
 
-# Technology top stories URL
-tech_url = "https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=WCneLj9e31CN4wBTLqA7RxndRuHC10eZ"
+# Technology top stories URL - PLEASE USE YOUR API HERE
+tech_url = "https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=YOURAPI"
 
 # Retrieve Technology top stories 
 response = requests.get(tech_url)
@@ -90,8 +90,8 @@ collection.insert_many(results)
 db = client.top_stories_nyt
 collection = db.opinions
 
-# Opinion top stories URL
-opinion_url = "https://api.nytimes.com/svc/topstories/v2/opinion.json?api-key=WCneLj9e31CN4wBTLqA7RxndRuHC10eZ"
+# Opinion top stories URL - PLEASE USE YOUR API HERE
+opinion_url = "https://api.nytimes.com/svc/topstories/v2/opinion.json?api-key=YOURAPI"
 
 # Retrieve Opinion top stories 
 response = requests.get(opinion_url)
